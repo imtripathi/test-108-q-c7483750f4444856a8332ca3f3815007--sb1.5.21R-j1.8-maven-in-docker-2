d5da1bb5dae09aa3248fbde80b01be28ab7013fd
User <User@DESKTOP-FS4A415>
2020-08-30 15:31:12 +0530

    commit

---
+++ src/main/java/org/codejude/sb/model/CreateUser.java
@@ -0,0 +1,15 @@
+package org.codejude.sb.model;
+
+public class CreateUser {
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+
+}
--- src/main/java/org/codejudge/sb/controller/AppController.java
+++ src/main/java/org/codejudge/sb/controller/AppController.java
@@ -1,5 +1,6 @@
 package org.codejudge.sb.controller;
 
+import org.codejude.sb.model.CreateUser;
 import org.codejude.sb.model.FriendList;
 import org.codejude.sb.model.FriendSuggestionList;
 import org.codejude.sb.model.PendingFriendRequest;
@@ -14,8 +15,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -31,9 +34,9 @@ public class AppController {
     @SuppressWarnings("static-access")
 	@ApiOperation("This create the username")
     @PostMapping("/create/{username}")
-    public ResponseEntity<UserResponse> create(@PathVariable(name ="username") String username ) throws UserCreationException {
+    public ResponseEntity<UserResponse> create(@RequestBody CreateUser  username ) throws UserCreationException {
     	UserResponse usrRes= new UserResponse();
-    	usrRes=appService.create(username);
+    	usrRes=appService.create(username.getUsername());
         return ResponseEntity.ok(usrRes).status(201).body(usrRes);
     }
     
@@ -58,7 +61,7 @@ public class AppController {
 
     @SuppressWarnings("static-access")
 	@ApiOperation("This gets the friend list of user")
-    @GetMapping("/friend/{friendlistfor}")
+    @GetMapping("/friends/{friendlistfor}")
     public ResponseEntity<FriendList> getFriendList(@PathVariable(name ="friendlistfor") String friendlistfor ) throws NoDataFoundException   {
     	FriendList res= new FriendList();
     	res=appService.getFriendList(friendlistfor);
