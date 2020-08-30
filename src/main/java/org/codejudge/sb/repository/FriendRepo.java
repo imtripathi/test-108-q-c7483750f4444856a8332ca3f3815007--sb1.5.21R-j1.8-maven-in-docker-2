244211084fa2d56c45396bbba107791d9194af90
User <User@DESKTOP-FS4A415>
2020-08-30 14:19:13 +0530

    c

--- src/main/java/org/codejudge/sb/repository/FriendRepo.java
+++ src/main/java/org/codejudge/sb/repository/FriendRepo.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface FriendRepo extends CrudRepository<Friends,Long>{
-	@Query(value = "select * from codejudge.friends where friend='1'", nativeQuery = true)
+	@Query(value = "select * from friends where friend='1'", nativeQuery = true)
 	List<Friends> getFriendSuggestion(String friendsufggestionfor);
 	
 	
--- src/main/java/org/codejudge/sb/repository/FriendReqRepo.java
+++ src/main/java/org/codejudge/sb/repository/FriendReqRepo.java
@@ -15,17 +15,17 @@ public interface FriendReqRepo extends CrudRepository<FriendRequest,Serializable
 	 @Query(value = "select * from friend_request where friend_req_from=?1 and accepted='0' ", nativeQuery = true)
 	List<FriendRequest> findfriendReq(String name);
 	 
-	 @Query(value = "select * from codejudge.friend_request where  friend_req_from=?1 and friend_req_to=?2", nativeQuery = true)
+	 @Query(value = "select * from friend_request where  friend_req_from=?1 and friend_req_to=?2", nativeQuery = true)
 	 List<FriendRequest> updateaccepted( String friend_req_from, String friend_req_to );
 
 	 
-	 @Query(value = "select * from codejudge.friend_request where friend_req_to=?1 and accepted='0'", nativeQuery = true)
+	 @Query(value = "select * from friend_request where friend_req_to=?1 and accepted='0'", nativeQuery = true)
 	 List<FriendRequest> getpendingFriendReq( String friend_req_to );
 	 
-	 @Query(value = "select * from codejudge.friend_request where friend_req_to=?1 and accepted='1'", nativeQuery = true)
+	 @Query(value = "select * from friend_request where friend_req_to=?1 and accepted='1'", nativeQuery = true)
 	 List<FriendRequest> getFriendList( String friend_req_to );
 
-	 @Query(value = "select * from codejudge.friend_request where accepted='1'", nativeQuery = true)
+	 @Query(value = "select * from friend_request where accepted='1'", nativeQuery = true)
 	List<FriendRequest> getFriendSuggestion(String friend_req_from);
 
 }
